4f7aeeb9fc504a1d43162bd99c16cf15198f9c98
ariflyra <113499794+ariflyra@users.noreply.github.com>
2022-09-14 14:40:05 +0700

    Update tampil.php

--- tampil.php
+++ tampil.php
@@ -1,1 +1,37 @@
 <!DOCTYPE html>
+<html>
+<head>
+  <title>login  Form</title>
+  <link  rel="stylesheet" href="style.css" type="text/css">
+ </heat>
+ <body>
+   
+ <!-- <h2>login From </h2> -->
+   
+ <from action="tampil.php" nethod="POST">
+   <h2>From Login </h2>
+   <div class="imgcontiner">
+       <img src="icon.png" alt="AVATAR" class="avatar">
+   </div>
+   
+   <div class="container">
+     <label><b>Username</b></label>
+      <input type="text"  placeholder="Masukkan  Username
+name="username" requidred>
+               
+       <label><b>Password</b></label>
+       <input type="password" placeholder="<Masukkan Password
+name="email"  required                        
+               
+     <button type="submit">Masuk</button>
+   <input type="chackbox" checked="cheked"><span>ingat Saya</spasi
+   </div>
+               
+   <div class="container"  style="background-color:#flflfl">
+     <button type="button" class="cancelbtn">Batal</button>
+     <span class="psw">Lupa <a href="#">password?</a></span>
+   </div>                        
+</from>
+     
+</body>
+</html>
